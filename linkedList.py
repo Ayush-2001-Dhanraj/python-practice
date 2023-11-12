d69b9b620bc96093a463c29d4f9e8f8a7ba8cc57
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2023-11-12 17:35:06 +0530

    Add Linked List

--- linkedList.py
+++ linkedList.py
@@ -27,15 +27,88 @@ class LinkedList:
         new_node.next_node = self.head
         self.head = new_node
 
+    def search(self, key):
+        current = self.head
+
+        while(current):
+            if current.data == key:
+                return current
+            current = current.next_node
+        return None
+    
+    def insert(self, data, index):
+        if index == 0:
+            self.prepend(data)
+        
+        if index > 0 and index <= self.size():
+            pos = 0
+            current = self.head
+            while pos != index -1:
+                current = current.next_node
+                pos += 1
+            
+            prev_node = current
+            next_node = prev_node.next_node
+
+            new_node = Node(data)
+
+            prev_node.next_node = new_node
+            new_node.next_node = next_node
+    
+    def delete(self, data):
+        prev_node = self.head
+        current = prev_node
+
+        while current and current.data is not data:
+            prev_node = current
+            current = current.next_node
+
+        if current:
+            prev_node.next_node = current.next_node
+
+            if current is self.head:
+                self.head = self.head.next_node
+        
+        return current
+    
+    def nodeAtIndex(self, index):
+        if index == 0:
+            return self.head
+        
+        if index > 0 and index < self.size():
+            pos = index
+            current = self.head
+            while pos > 0:
+                current = current.next_node
+                pos -= 1
+            return current
+        
+    def deleteAtIndex(self, index):
+        current = self.head
+        if index == 0:
+            self.head = current.next_node
+            return current
+        
+        if index > 0 and index < self.size():
+            pos = index
+            while pos > 1:
+                pos -= 1
+                current = current.next_node
+
+            deletedNode = current.next_node
+            current.next_node = current.next_node.next_node
+            
+            return deletedNode       
+
     def __repr__(self):
         current = self.head
         nodes = []
 
         while current:
             if current == self.head:
-                nodes.append("[Head]: %s" % current.data)
+                nodes.append("Head: [%s]" % current.data)
             elif current.next_node == None:
-                nodes.append("[Tail]: %s" % current.data)
+                nodes.append("[%s] : Tail" % current.data)
             else: 
                 nodes.append("[%s] " % current.data)
             current = current.next_node
