69729089cc7f7b61ef766d3cc1ed5ed593b3b245
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2023-11-14 13:37:58 +0530

    Add layer class

--- neural_network.py
+++ neural_network.py
@@ -1,22 +1,33 @@
 import numpy as np
 
-inputs = [[1, 2, 3, 2.5],
+# initialize seed
+np.random.seed(0)
+
+X = [[1, 2, 3, 2.5],
           [2.0, 5.0, -1.0, 2.0],
           [-1.5, 2.7, 3.3, -0.8]]
 
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        """
+        n_inputs represents the no of features
+        n_neurons represents the no of neurons we want in this particular layer
 
-biases = [2, 3, 0.5]
+        weights are inputs x no of neurons - helps avoid transpose operation in dot product
+        weights range between -1 to +1 so tht they are close to each other and NN doesn't explode
+        
+        biases is set to zero initially and if we encounter error where the entore output of
+        NN is zero we cqan intialize it to some value to avoid dead ANN
+        """
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-weights2 = [[0.1, -0.14, 0.5],
-           [-0.5, 0.12, -0.33],
-           [-0.44, 0.73, -0.13]]
+layer1 = Layer_Dense(4, 5)
+layer2 = Layer_Dense(5, 2)
 
-biases2 = [-1, 2, -0.5]
-
-layer1_op = np.dot(inputs, np.array(weights).T) + biases
-layer2_op = np.dot(layer1_op, np.array(weights2).T) + biases2
-
-print (layer2_op)
+layer1.forward(X)
+print(layer1.output)
+layer2.forward(layer1.output)
+print(layer2.output)
