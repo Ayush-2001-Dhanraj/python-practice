f13e3c3da4a1e42eb0bb43fab7eacbe8685d5250
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2023-11-14 15:15:31 +0530

    ReLU activation fn

--- neural_network.py
+++ neural_network.py
@@ -1,11 +1,12 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
 
+nnfs.init()
 # initialize seed
-np.random.seed(0)
+# np.random.seed(0)
 
-X = [[1, 2, 3, 2.5],
-          [2.0, 5.0, -1.0, 2.0],
-          [-1.5, 2.7, 3.3, -0.8]]
+X, y = spiral_data(5, 3)  
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
@@ -24,10 +25,15 @@ class Layer_Dense:
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
-layer1 = Layer_Dense(4, 5)
-layer2 = Layer_Dense(5, 2)
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
 
+
+
+layer1 = Layer_Dense(2, 5)
+activarion1 = Activation_ReLU()
 layer1.forward(X)
-print(layer1.output)
-layer2.forward(layer1.output)
-print(layer2.output)
+# print(layer1.output)
+activarion1.forward(layer1.output)
+print(activarion1.output)
